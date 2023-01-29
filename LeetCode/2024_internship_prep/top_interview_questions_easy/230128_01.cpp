80338a02bd2e4254ddfd8d86b6623ebad8fa3b45
HOzy-Joon <danielisdan@naver.com>
2023-01-30 02:07:25 +0900

    230129 covered

--- LeetCode/2024_internship_prep/top_interview_questions_easy/230128_01.cpp
+++ LeetCode/2024_internship_prep/top_interview_questions_easy/230128_01.cpp
@@ -1,53 +1,21 @@
 #include <vector>
-#include <unordered_set>
+#include <unordered_map>
 using namespace std;
 
-bool isValidSudoku(vector<vector<char>>& board) {
-    int i, j, n, k;
-    unordered_set<char> S;
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> M;
+    vector<int> result;
     
-    for (i=0; i<9; i++){
-        for (n=0; n<9; n++){
-            if (board[i][n] != '.'){
-                if (S.find(board[i][n]) == S.end()){
-                    S.insert(board[i][n]);
-                } else {
-                    return false;
-                }
-            }
+    for (int i=0; i<nums.size(); i++){
+        if (M.find(target - nums[i]) == M.end()){
+            M.insert({nums[i], i});
+        } else {
+            result.push_back(M.find(target - nums[i])->second);
+            result.push_back(i);
+            break;
         }
-        S.clear();
     }
     
-    for (j=0; j<9; j++){
-        for (k=0; k<9; k++){
-            if (board[k][j] != '.'){
-                if (S.find(board[k][j]) == S.end()){
-                    S.insert(board[k][j]);
-                } else {
-                    return false;
-                }
-            }
-        }
-        S.clear();
-    }
+    return result;
     
-    for (i=0; i<3; i++){
-        for (j=0; j<3; j++){
-            for (n=0; n<3; n++){
-                for (k=0; k<3; k++){
-                    if (board[3*i+n][3*j+k] != '.'){
-                        if (S.find(board[3*i+n][3*j+k]) == S.end()){
-                            S.insert(board[3*i+n][3*j+k]);
-                        } else {
-                            return false;
-                        }
-                    }
-                }
-            }
-            S.clear();
-        } 
-    }
-    
-    return true;
 }
