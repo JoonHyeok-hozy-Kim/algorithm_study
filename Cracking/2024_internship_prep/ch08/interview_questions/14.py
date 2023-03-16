c24be916934ffb90db6043e1854cd16352c5e962
HOzy-Joon <danielisdan@naver.com>
2023-03-17 01:03:25 +0900

    230317 covered

--- Cracking/2024_internship_prep/ch08/interview_questions/14.py
+++ Cracking/2024_internship_prep/ch08/interview_questions/14.py
@@ -1,55 +1,55 @@
-from itertools import permutations
-from copy import deepcopy
-
-def parse_expression(exp):
-    symbols = []
-    operators = []
-    for c in exp:
-        if c in ('&', '|', '^'):
-            operators.append(c)
+def countEval(expr, value, M={}):
+    if len(expr) == 1:
+        if (expr == '1' and value) or (expr == '0' and not value):
+            return 1
         else:
-            symbols.append(int(c))
-    return symbols, operators
-
-def operate(symbols, operators, oi):
-    if operators[oi] == '&':
-        temp = symbols[oi] & symbols[oi+1]
-    elif operators[oi] == '|':
-        temp = symbols[oi] | symbols[oi+1]
-    elif operators[oi] == '^':
-        temp = symbols[oi] ^ symbols[oi+1]
-    symbols.pop(oi)
-    symbols[oi] = temp
-    operators.pop(oi)
-
-def show_expression(s, o):
-    idx = 0
-    while idx < len(o):
-        print(s[idx], end="")
-        print(o[idx], end="")
-        idx += 1
-    print(s[idx])
-
-def boolean_evaluation(exp, desired_result):
-    symbols, operators = parse_expression(exp)
-    # show_expression(symbols, operators)
-    return _inner(symbols, operators, desired_result)
-
-def _inner(symbols, operators, desired_result, depth=0):
-    if len(symbols) == 1 and symbols[0] == desired_result:
-        return 1
+            return 0
     
-    for i in range(depth):
-        print(" ", end="")
-    show_expression(symbols, operators)
     result = 0
-    for i in range(len(operators)):
-        cs, co = deepcopy(symbols), deepcopy(operators)
-        operate(cs, co, i)
-        result += _inner(cs, co, desired_result, depth+1)
+    for i in range(1, len(expr), 2):
+        operator = expr[i]
+        left = expr[:i]
+        right = expr[i+1:]
+        
+        tflr = [None] * 4
+        cnt = 0
+        for tf in (True, False):
+            for lr in (left, right):
+                if (tf, lr) not in M:
+                    M[(tf, lr)] = countEval(lr, tf, M)
+                tflr[cnt] = M[(tf, lr)]
+                
+        left_true, right_true, left_false, right_false = tflr
+
+        if operator == '&':
+            if value:
+                result += left_true * right_true
+            else:
+                result += left_false * right_true
+                result += left_true * right_false
+                result += left_false * right_false
+        elif operator == '|':
+            if value:
+                result += left_true * right_true
+                result += left_false * right_true
+                result += left_true * right_false
+            else:
+                result += left_false * right_false
+        elif operator == '^':
+            if value:
+                result += left_false * right_true
+                result += left_true * right_false
+            else:
+                result += left_true * right_true
+                result += left_false * right_false
+        else:
+            raise Exception("Undefined operator.")
+    
+    # print('{} / {} / {}'.format(expr, value, result))
     return result
 
 
 if __name__ == '__main__':
-    # print(boolean_evaluation("1^0|0|1", 0))
-    print(boolean_evaluation("0&0&0&1^1|0", 1))
+    print(countEval("1|0&0", False))
+    print(countEval("1^0|0|1", False))
+    print(countEval("0&0&0&1^1|0", True))
